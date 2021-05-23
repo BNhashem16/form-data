c610e8492cef86a3c2691e5dfc5fd3743ddd3437
Ahmed Hashem <a.hashem@thetailorsdev.com>
2021-05-23 11:11:12 +0200

    first commit

--- src/FormData.php
+++ src/FormData.php
@@ -4,26 +4,15 @@ namespace Bnhashem\FormData;
 
 use Illuminate\Support\Facades\Schema;
 
-    function data($model)
+class FormData
+{
+    public function old($model)
     {
-        $route = request()->route()->getName();
-
-        $bindKey = $model::$bindKey ?? 'id';
-
-        if (request()->routeIs('*.edit')) {
-            $route = str_replace('edit', 'update', $route);
-            $route = route($route, $model->$bindKey);
-        } else {
-            $route = str_replace('create', 'store', $route);
-            $route = route($route);
-        }
-
         $columns = Schema::getColumnListing(($model)->getTable());
         foreach ($columns as $column) {
-            $data[$column] = $model->id == null ? old($column) : $model->$column;
+            $data[$column] = old($column);
         }
-
-        $data['route'] = $route;
 
         return $data;
     }
+}
