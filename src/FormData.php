7d3ba8fd90ded470ad90871e6a1f84355207970e
BNHashem <a.hashem@thetailorsdev.com>
2021-06-15 03:57:32 +0200

    Testing

--- src/FormData.php
+++ src/FormData.php
@@ -9,9 +9,21 @@ class FormData
     public static function old($model): array
     {
         $columns = Schema::getColumnListing(($model)->getTable());
+        $data = [];
+
         foreach ($columns as $column) {
             $data[$column] = old($column);
+
+            if (array_key_exists($column, $model::$JSONCOLUMNS)) {
+                // $model::$JSONCOLUMNS[$column] = collect(['ar' => old($column), 'en' => old($column)]);
+
+                $data[$column] = $model::$JSONCOLUMNS[$column];
+                // $data[$column] = $model::$JSONCOLUMNS[$column];
+            }
         }
+        dd($data);
+        dd($model::$JSONCOLUMNS[$column]);
+
 
         return $data;
     }
