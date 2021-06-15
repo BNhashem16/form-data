564fe72ca17fd72fc0509fa697227f3fc8ebbede
BNHashem <a.hashem@thetailorsdev.com>
2021-06-16 01:55:36 +0200

    Testing is Done

--- src/FormData.php
+++ src/FormData.php
@@ -9,20 +9,13 @@ class FormData
     public static function old($model): array
     {
         $columns = Schema::getColumnListing(($model)->getTable());
-        $data = [];
-
         foreach ($columns as $column) {
             $data[$column] = old($column);
 
-            if (array_key_exists($column, $model::$JSONCOLUMNS)) {
-                // $model::$JSONCOLUMNS[$column] = collect(['ar' => old($column), 'en' => old($column)]);
-
-                $data[$column] = $model::$JSONCOLUMNS[$column];
-                // $data[$column] = $model::$JSONCOLUMNS[$column];
+            if (self::jsonColumnsIsset($model)) {
+                self::jsonColumns($column, $model);
             }
         }
-        dd($data);
-        dd($model::$JSONCOLUMNS[$column]);
 
 
         return $data;
@@ -36,5 +29,24 @@ class FormData
         }
 
         return $data;
+    }
+
+    public static function jsonColumns(string $column, object $model)
+    {
+        if (array_key_exists($column, $model::$JSONCOLUMNS)) {
+            $json_column_keys = array_flip($model::$JSONCOLUMNS[$column]);
+
+            foreach ($json_column_keys as $json_column_key) {
+                $data[$column][$json_column_key] = old($column.$json_column_key);
+            }
+
+            return $data;
+        }
+    }
+
+
+    public static function jsonColumnsIsset($model): bool
+    {
+        return isset($model::$JSONCOLUMNS) ? true : false;
     }
 }
